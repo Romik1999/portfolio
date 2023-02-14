2b2c9073af3d515a5918d70426118e6124bc0fa0
roman <usankin.roma@gmail.com>
2023-02-14 12:57:44 +0400

    separate main page in components, create projects page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import './styles/reset.css'
+import './styles/main.css'
+
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, React JS!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Navbar/>
+
+            {/*<Home/>*/}
+            <Projects/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
