24add5822e7c0f54d79cb2d28940a7ae105e2dcc
roman <usankin.roma@gmail.com>
2023-02-14 13:24:20 +0400

    fix styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
 
             {/*<Home/>*/}
             <Projects/>
+            {/*<Contacts/>*/}
             <Footer/>
         </div>
     );
