32b3ba175b8563882653f0f755209eb7f91926ad
roman <usankin.roma@gmail.com>
2023-02-14 13:58:08 +0400

    make project props from file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,5 @@
 import Project from "../components/Project/Project";
-import project01 from './../img/projects/01.jpg'
-import project02 from './../img/projects/02.jpg'
-import project03 from './../img/projects/03.jpg'
-import project04 from './../img/projects/04.jpg'
-import project05 from './../img/projects/05.jpg'
-import project06 from './../img/projects/06.jpg'
+import {projects} from "../helpers/projectsList";
 
 const Projects = () => {
     return (
@@ -13,9 +8,14 @@ const Projects = () => {
                 <div className="container">
                     <h2 className="title-1">Projects</h2>
                     <ul className="projects">
-                      <Project/>
-                      <Project/>
-                      <Project/>
+
+                        {
+                            projects.map((project) => {
+                                return (
+                                    <Project title={project.title} img={project.img} />
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </main>
