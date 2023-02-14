c7f7c0175effcc46487a99fe5f9d89bef2e371d6
roman <usankin.roma@gmail.com>
2023-02-14 13:38:49 +0400

    add projects arr

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,4 @@
+import Project from "../components/Project/Project";
 import project01 from './../img/projects/01.jpg'
 import project02 from './../img/projects/02.jpg'
 import project03 from './../img/projects/03.jpg'
@@ -12,38 +13,9 @@ const Projects = () => {
                 <div className="container">
                     <h2 className="title-1">Projects</h2>
                     <ul className="projects">
-                        <li className="project">
-                            <a href="./project-page.html">
-                                <img src={project01} alt="Project img" className="project__img"/>
-                                <h3 className="project__title">Gaming streaming portal</h3>
-                            </a>
-                        </li>
-                        <li className="project">
-                            <a href="./project-page.html">
-                                <img src={project02} alt="Project img" className="project__img"/>
-                                <h3 className="project__title">Video service</h3>
-                            </a>
-                        </li>
-                        <li className="project">
-                            <a href="./project-page.html">
-                                <img src={project03} alt="Project img" className="project__img"/>
-                                <h3 className="project__title">Video portal</h3>
-                            </a>
-                        </li>
-
-                        <li className="project">
-                            <img src={project04} alt="Project img" className="project__img"/>
-                            <h3 className="project__title">Dating app</h3>
-                        </li>
-                        <li className="project">
-                            <img src={project05} alt="Project img" className="project__img"/>
-                            <h3 className="project__title">Landing</h3>
-                        </li>
-                        <li className="project">
-                            <img src={project06} alt="Project img" className="project__img"/>
-                            <h3 className="project__title">Gaming community</h3>
-                        </li>
-
+                      <Project/>
+                      <Project/>
+                      <Project/>
                     </ul>
                 </div>
             </main>
