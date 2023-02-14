3cdd0d97b08f4bc9f6f032b09f9cda35e34ce552
roman <usankin.roma@gmail.com>
2023-02-14 14:41:25 +0400

    fix scroll bug

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ import Project from "./pages/Project";
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
 
+import ScrollToTop from "./utils/scrollToTop";
+
 function App() {
     return (
         <div className="App">
 
             <Router>
+                <ScrollToTop/>
                 <Navbar/>
                 <Routes>
                     <Route path="/" element={<Home/>}></Route>
